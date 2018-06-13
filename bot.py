a32313a7ecb189d6c92fceb9299b39595bd94e8f
Mariobob <35691890+Mariobob@users.noreply.github.com>
2018-06-13 13:46:08 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,9 @@ import discord
 from discord.ext import commands
 import time
 import os
-
+import inspect
+import io
+import textwrap
 
 
 
@@ -10,7 +12,14 @@ bot = commands.Bot(command_prefix='e.', description="An easy to use discord bot"
 bot.load_extension("cogs.fun")
 bot.load_extension("cogs.utility")
 bot.load_extension("cogs.mod")
+bot._last_result = None
 
+def dev_check(id):
+    with open('data/devs.json') as f:
+        devs = json.load(f)
+        if id in devs:
+            return True
+        return False
 
 
 @bot.event
@@ -21,5 +30,88 @@ async def on_ready():
 
 
 
+@bot.command(name='eval')
+async def _eval(ctx, *, body):
+    """Evaluates python code"""
+    if not dev_check(ctx.author.id):
+        return await ctx.send("You cannot use this because you are not a developer.")
+    env = {
+        'ctx': ctx,
+        'channel': ctx.channel,
+        'author': ctx.author,
+        'guild': ctx.guild,
+        'message': ctx.message,
+        '_': bot._last_result,
+        'source': inspect.getsource
+    }
+
+    env.update(globals())
+
+    body = cleanup_code(body)
+    stdout = io.StringIO()
+    err = out = None
+
+    to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+    def paginate(text: str):
+        '''Simple generator that paginates text.'''
+        last = 0
+        pages = []
+        for curr in range(0, len(text)):
+            if curr % 1980 == 0:
+                pages.append(text[last:curr])
+                last = curr
+                appd_index = curr
+        if appd_index != len(text) - 1:
+            pages.append(text[last:curr])
+        return list(filter(lambda a: a != '', pages))
+
+    try:
+        exec(to_compile, env)
+    except Exception as e:
+        err = await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+        return await ctx.message.add_reaction('\u2049')
+
+    func = env['func']
+    try:
+        with redirect_stdout(stdout):
+            ret = await func()
+    except Exception as e:
+        value = stdout.getvalue()
+        err = await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+    else:
+        value = stdout.getvalue()
+        if ret is None:
+            if value:
+                try:
+
+                    out = await ctx.send(f'```py\n{value}\n```')
+                except:
+                    paginated_text = paginate(value)
+                    for page in paginated_text:
+                        if page == paginated_text[-1]:
+                            out = await ctx.send(f'```py\n{page}\n```')
+                            break
+                        await ctx.send(f'```py\n{page}\n```')
+        else:
+            bot._last_result = ret
+            try:
+                out = await ctx.send(f'```py\n{value}{ret}\n```')
+            except:
+                paginated_text = paginate(f"{value}{ret}")
+                for page in paginated_text:
+                    if page == paginated_text[-1]:
+                        out = await ctx.send(f'```py\n{page}\n```')
+                        break
+                    await ctx.send(f'```py\n{page}\n```')
+
+    if out:
+        await ctx.message.add_reaction('\u2705')  # tick
+    elif err:
+        await ctx.message.add_reaction('\u2049')  # x
+    else:
+        await ctx.message.add_reaction('\u2705')
+	
+	
 
 bot.run(os.environ.get("TOKEN"))
