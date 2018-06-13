6dfbd91e347fd962740744f896622ad687ae03ec
lovish2001 <36467324+lovish2001@users.noreply.github.com>
2018-06-13 23:04:29 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord 
+from discord.ext import commands
+import time
+import os
+
+
+
+
+bot = commands.Bot(command_prefix='!', description="Nothing")
+bot.load_extension("cogs.fun")
+bot.load_extension("cogs.utility")
+bot.load_extension("cogs.mod")
+
+
+
+@bot.event
+async def on_ready():
+	print('Logged in as'+ bot.user.name)
+	print(bot.user.id)
+	print('------')
+
+
+
+
+bot.run(os.environ.get("TOKEN"))
