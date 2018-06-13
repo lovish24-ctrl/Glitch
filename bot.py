7caff0a28be6c428f62326061503d2545f2c985c
Mariobob <35691890+Mariobob@users.noreply.github.com>
2018-06-13 13:43:12 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import os
 
 
 
-bot = commands.Bot(command_prefix='!', description="Nothing")
+bot = commands.Bot(command_prefix='e.', description="An easy to use discord bot")
 bot.load_extension("cogs.fun")
 bot.load_extension("cogs.utility")
 bot.load_extension("cogs.mod")
